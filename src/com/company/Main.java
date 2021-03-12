902530b7bdd8dff75691c644092d087f93fa95d9
Ani Manukyan <animanukyan95@gmail.com>
2021-03-12 18:06:49 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+	public static void main(String args[]) {
+
+
+		int x=5;
+		int y=7;
+		System.out.println("x=" + x + " y=" + y);
+		System.out.printf("x=%d; y=%d \n",  x, y);
+
+		String name = "Ani";
+		int age = 25;
+		float height = 1.67f;
+		System.out.printf("Name: %s Age: %d Height: %.2f", name, age, height);
+
+
+
+	}
+}
---
+++ src/com/ex/Training.java
@@ -0,0 +1,7 @@
+package com.ex;
+
+public class Training {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
